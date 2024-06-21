1c736080659e80e554ce9bd3a4fbcadd9f5105a8
Cselfy1 <cselfymain@gmail.com>
2024-06-21 17:20:33 +0300

    5

---
+++ homework/5/main3.py
@@ -0,0 +1,8 @@
+num = int(input("Enter the num: ")) 
+
+if num > 0: 
+    print("Number is positive")
+elif num < 0: 
+    print("Number is negative")
+else: 
+    print("Number is equal to zero")
---
+++ homework/5/main4.py
@@ -0,0 +1,10 @@
+a = int(input("1st num: ")) 
+b = int(input("2nd num: ")) 
+
+if a == b:
+    print("the numbers are equal") 
+else: 
+    if a < b:
+        print(a, b)
+    else: 
+        print(b, a)
